78393d85d26c5e90aaee266d59603d8559086693
Carlos <Carlos@xtrene.com>
2021-06-21 19:50:50 +0200

    Agregar archivos de proyecto.

---
+++ Tarea04_10/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Tarea04_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            Console.WriteLine("Números pares o divisibles entre 3 del 1 al 100: ");
+            while (num < 100)
+            {
+                num++;
+                if (num % 2 == 0 || num%3 == 0)
+                {
+                    Console.Write(" " + num);
+                }
+
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
